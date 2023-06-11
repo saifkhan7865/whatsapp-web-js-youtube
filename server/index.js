4979185910cd4543e505648dbd2f495105f83a5b
Mohammed Khan <60229505+saifkhan7865@users.noreply.github.com>
2023-06-11 16:12:03 +0530

    initial commit for whatsapp web js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 // create a basic node js with express app
 const express = require("express");
-const { Client, LocalAuth } = require("whatsapp-web.js");
+const { Client, RemoteAuth } = require("whatsapp-web.js");
 const app = express();
 const port = 3001;
 const http = require("http");
 const server = http.createServer(app);
+const { MongoStore } = require("wwebjs-mongo");
+const mongoose = require("mongoose");
+const MONGO_URI =
+  "mongodb+srv://Saif:Arhaan123@cluster0.mj6hd.mongodb.net/test";
+let store;
+
+mongoose.connect(MONGO_URI).then(() => {
+  console.log("hello connected mongoDB");
+  store = new MongoStore({ mongoose: mongoose });
+});
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
@@ -26,8 +36,10 @@ const createWhatsappSession = (id, socket) => {
     puppeteer: {
       headless: false,
     },
-    authStrategy: new LocalAuth({
+    authStrategy: new RemoteAuth({
       clientId: id,
+      store: store,
+      backupSyncIntervalMs: 300000,
     }),
   });
 
@@ -45,6 +57,43 @@ const createWhatsappSession = (id, socket) => {
     console.log("Client is ready!");
     allSessionsObject[id] = client;
     socket.emit("ready", { id, message: "Client is ready!" });
+  });
+
+  client.on("remote_session_saved", () => {
+    console.log("remote_session_saved");
+    socket.emit("remote_session_saved", {
+      message: "remote_session_saved",
+    });
+  });
+
+  client.initialize();
+};
+
+const getWhatsappSession = (id, socket) => {
+  const client = new Client({
+    puppeteer: {
+      headless: false,
+    },
+    authStrategy: new RemoteAuth({
+      clientId: id,
+      store: store,
+      backupSyncIntervalMs: 300000,
+    }),
+  });
+
+  client.on("ready", () => {
+    console.log("client is ready");
+    socket.emit("ready", {
+      id,
+      message: "client is ready",
+    });
+  });
+
+  client.on("qr", (qr) => {
+    socket.emit("qr", {
+      qr,
+      message: "your got logged out and here is QR code",
+    });
   });
 
   client.initialize();
@@ -66,6 +115,12 @@ io.on("connection", (socket) => {
     console.log(data);
     const { id } = data;
     createWhatsappSession(id, socket);
+  });
+
+  socket.on("getSession", (data) => {
+    console.log(data);
+    const { id } = data;
+    getWhatsappSession(id, socket);
   });
 
   socket.on("getAllChats", async (data) => {
